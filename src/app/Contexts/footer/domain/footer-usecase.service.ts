a75794a5092e611ac50a675c823ec7d25dd321e8
HUGO-BENDER <HUGO.BENDER@berger-levrault.com>
2022-09-30 20:48:53 +0200

    bk02

--- src/app/Contexts/footer/domain/footer-usecase.service.ts
+++ src/app/Contexts/footer/domain/footer-usecase.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FooterGateway } from './footer-gateway';
-import { Metadata } from './footer-model';
+import { Metadata,FooterBlock } from './footer-model';
 
 @Injectable({
   providedIn: 'root',
